f07a5368b8884a3ef81620398d8eecc79b43a606
edihiho <63027845+edihiho@users.noreply.github.com>
2025-03-17 16:07:10 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -648,4 +648,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
